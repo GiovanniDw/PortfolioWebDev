1fcf0730d80091d349295de91d6b38e9cc9f4dff
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2023-03-13 19:32:33 +0100

    Updates

---
+++ assets/AboutView-1cf7afa3.js
@@ -0,0 +1,16 @@
+import { o as openBlock, c as createElementBlock, a as createVNode, b as _sfc_main$1 } from "./index-79f0fa8b.js";
+const _hoisted_1 = { class: "about" };
+const _sfc_main = {
+  __name: "AboutView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("main", _hoisted_1, [
+        createVNode(_sfc_main$1)
+      ]);
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=AboutView-1cf7afa3.js.map
