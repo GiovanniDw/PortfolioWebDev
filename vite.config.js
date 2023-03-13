ad159533e6de718fe99c4e6da760148d2342e42f
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2023-03-13 19:18:15 +0100

    add build options

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,25 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PortfolioWebDev/',
   plugins: [vue()],
   resolve: {
-      alias: {
+    alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+    }
   },
   css: {
-      devSourcemap: true
+    devSourcemap: true
     },
-    server: {
+  server: {
     port: 3000,
   },
   preview: {
     port: 8000,
+  },
+  build: {
+    outDir: 'docs',
+    sourcemap: true,
+    minify: false,
   }
 })
