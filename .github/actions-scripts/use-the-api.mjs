3619c0274f2a2d20ccb7ac84368022d76cca8a80
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2023-03-15 18:16:41 +0100

    change action

--- .github/actions-scripts/use-the-api.mjs
+++ .github/actions-scripts/use-the-api.mjs
@@ -1,24 +1,7 @@
-on:
-  workflow_dispatch:
-jobs:
-  use_api_via_script:
-    runs-on: ubuntu-latest
-    permissions: {}
-    steps:
-      - name: Check out repo content
-        uses: actions/checkout@v3
+import { Octokit } from 'octokit'
 
-      - name: Setup Node
-        uses: actions/setup-node@v3
-        with:
-          node-version: '16.17.0'
-          cache: npm
+const octokit = new Octokit({
+  auth: process.env.TOKEN
+})
 
-      - name: Install dependencies
-        run: npm install octokit
-
-      - name: Run script
-        env:
-          TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        run: |
-          node .github/actions-scripts/use-the-api.mjs
+await octokit.request('GET /octocat', {})
